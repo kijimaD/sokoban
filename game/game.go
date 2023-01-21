6622caca4cc4a60b6e00e7614658f80bae45f314
kijimaD <norimaking777@gmail.com>
2023-01-22 00:18:23 +0900

    feat: string functions

--- game/game.go
+++ game/game.go
@@ -3,35 +3,66 @@ package game
 import (
 	"fmt"
 	"math"
-	"strconv"
 )
 
+// タイル
 type Tile struct {
 	Kind int
 }
 
+const (
+	WALL  = `#`
+	FLOOR = `.`
+)
+
+func (t Tile) String() string {
+	if t.Kind == 0 {
+		return WALL
+	} else if t.Kind == 1 {
+		return FLOOR
+	} else {
+		return "?"
+	}
+}
+
+// (x, y)の形でアクセスできる
+// 一覧表示できる
+// 二次元配列に変換すればよさげ。[0][1]に代入、みたいにして変換できる。
 type Stage struct {
 	Tiles  map[Pos]Tile
 	Player Entity
 }
 
+// タイルの上にあるもの。プレイヤーや荷物など、移動する
 type Entity struct {
 	Pos Pos
 }
 
+// マップ上の座標
 type Pos struct {
 	X int
 	Y int
 }
 
-func (s Stage) Show() {
-	for k, _ := range s.Tiles {
-		fmt.Printf("(%#v, %#v)", k.X, k.Y)
+func (s Stage) String() string {
+	result := ""
+
+	l := len(s.Tiles)
+	w := int(math.Sqrt(float64(l)))
+
+	for i := 0; i-1 < w; i++ {
+		for j := 0; j-1 < w; j++ {
+			tile := s.Tiles[Pos{X: i, Y: j}]
+			result = result + fmt.Sprintf("%s", tile)
+		}
+		result = result + "\n"
 	}
+
+	return result
 }
 
-// 画面表示するのに使う
-func (s Stage) ToSlice() {
+// 二次元配列に変換する
+func (s Stage) ToSlice() [][]Tile {
 	l := len(s.Tiles)
 	w := int(math.Sqrt(float64(l)))
 
@@ -42,24 +73,13 @@ func (s Stage) ToSlice() {
 
 	for i := 0; i < w; i++ {
 		for j := 0; j < w; j++ {
-			arr[i][j] = Tile{Kind: 1}
+			tile := s.Tiles[Pos{X: i, Y: j}]
+			arr[i][j] = Tile{Kind: tile.Kind}
 		}
 	}
 
-	fmt.Printf("\n")
-	for i := 0; i < w; i++ {
-		for j := 0; j < w; j++ {
-			tile := arr[i][j]
-			char := strconv.Itoa(tile.Kind)
-			fmt.Printf("%s ", char)
-		}
-		fmt.Printf("\n")
-	}
+	return arr
 }
-
-// (x, y)の形でアクセスできる
-// 一覧表示できる
-// 二次元配列に変換すればよさげ。[0][1]に代入、みたいにして変換できる。
 
 func InitStage() Stage {
 	tiles := map[Pos]Tile{
--- game/game_test.go
+++ game/game_test.go
@@ -2,6 +2,8 @@ package game
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // ....
@@ -11,6 +13,31 @@ import (
 
 func TestInit(t *testing.T) {
 	s := InitStage()
-	s.Show()
+	s.String()
 	s.ToSlice()
+}
+
+func TestTileString(t *testing.T) {
+	assert.Equal(t, WALL, Tile{Kind: 0}.String())
+	assert.Equal(t, FLOOR, Tile{Kind: 1}.String())
+}
+
+func TestStageString(t *testing.T) {
+	s := InitStage()
+	expect := `.##
+.##
+###
+`
+	assert.Equal(t, expect, s.String())
+}
+
+func TestToSlice(t *testing.T) {
+	s := InitStage()
+
+	expect := [][]Tile{
+		{Tile{Kind: 1}, Tile{Kind: 0}},
+		{Tile{Kind: 1}, Tile{Kind: 0}},
+	}
+
+	assert.Equal(t, expect, s.ToSlice())
 }
